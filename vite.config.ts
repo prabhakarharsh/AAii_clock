b1b29fc655a17d54a666b59881e20110efc0aa2e
prabhakarharsh <prabhakarharsh2007@gmail.com>
2026-03-20 23:13:24 +0530

    fix: update base URL for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AAii_clock/',
+  base: '/',
   server: {
     port: 5174,
   },
